bc42cbd9e353807136de2374180bf8e82d570725
Søren Døssing <netcom@sauber.net>
2024-12-05 13:07:51 +0100

    Back exposes assets

--- src/backend/backend.ts
+++ src/backend/backend.ts
@@ -1,24 +1,24 @@
-import { CachingBackend, DiskBackend } from "📚/storage/mod.ts";
+import { Asset, CachingBackend, DiskBackend } from "📚/storage/mod.ts";
 import { Community } from "📚/repository/mod.ts";
-import { Ranking } from "📚/ranking/mod.ts";
-import { Model } from "📚/ranking/model.ts";
-import type { NetworkData } from "@sauber/neurons";
+import { Config } from "📚/config/config.ts";
+import { NetworkData } from "@sauber/neurons";
+
+// Asset names
+const rankingAsset = "ranking.network";
 
 export class Backend {
   private readonly repo: CachingBackend;
+  public readonly config: Config;
+  public readonly ranking: Asset<NetworkData>;
 
   constructor(readonly diskpath: string) {
     const disk = new DiskBackend(diskpath);
     this.repo = new CachingBackend(disk);
+    this.config = new Config(this.repo);
+    this.ranking = new Asset<NetworkData>(rankingAsset, this.repo);
   }
 
   public get community(): Community {
     return new Community(this.repo);
-  }
-
-  public async ranking(): Promise<Ranking> {
-    const data = await this.repo.retrieve("ranking.network") as NetworkData;
-    const model = Model.import(data);
-    return new Ranking(model);
   }
 }
