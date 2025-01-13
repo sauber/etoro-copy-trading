4605edfe07ed3e4af524e09836e8a3adf27ef6cd
Søren Døssing <netcom@sauber.net>
2025-01-13 15:03:55 +0100

    Strategy imports

--- src/trading/simulation_bin.ts
+++ src/trading/simulation_bin.ts
@@ -8,14 +8,9 @@ import {
 import { Assets } from "📚/assets/assets.ts";
 import { type Parameters } from "📚/trading/types.ts";
 import { Loader } from "📚/trading/loader.ts";
-import { Ranking } from "📚/ranking/mod.ts";
-import {
-  CascadeStrategy,
-  RankingStrategy,
-  RSIStrategy,
-  SizingStrategy,
-  WeekdayStrategy,
-} from "📚/strategy/mod.ts";
+import { Ranking, RankingStrategy } from "📚/ranking/mod.ts";
+import { CascadeStrategy, SizingStrategy } from "📚/strategy/mod.ts";
+import { RSIStrategy, WeekdayStrategy } from "📚/timing/mod.ts";
 
 // Repo
 const path: string = Deno.args[0];
