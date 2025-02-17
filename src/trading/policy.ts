ec5ae53c0ee7fe8012db4d6089136afa214a699b
Søren Døssing <netcom@sauber.net>
2025-02-17 19:52:57 +0100

    Only increase positions by gap to target

--- src/trading/policy.ts
+++ src/trading/policy.ts
@@ -45,7 +45,7 @@ export class Policy implements Strategy {
   }
 
   /** Rank of investor at bar */
-  private rank(instrument: Instrument, bar: Bar): number | undefined {
+  private confidence(instrument: Instrument, bar: Bar): number | undefined {
     if ("investor" in instrument) {
       return this.ranking.predict(instrument["investor"] as Investor, bar);
     }
@@ -56,6 +56,106 @@ export class Policy implements Strategy {
     const rsi = this.chart(instrument).bar(bar + 2);
     return rsi;
   }
+
+  /** Sort each positions into one of these categories:
+   *
+   * Sell Opportunity: Close now to take profit
+   * Buy opportunity: Increase position if instrument is desired and under-invested
+   * Keep
+   */
+  private close_classification(
+    context: StrategyContext,
+  ): [CloseOrders, CloseOrders, CloseOrders] {
+    const close: CloseOrders = [];
+    const increase: CloseOrders = [];
+    const keep: CloseOrders = [];
+    const targetAmount = context.value * this.position_size;
+
+    for (const co of context.closeorders) {
+      // Candidates for taking profit
+      const timing = this.timing(co.position.instrument, context.bar);
+      if (timing >= this.sell_threshold) {
+        close.push(co);
+        continue;
+      }
+
+      // Candidates to increase position size
+      if (timing <= this.buy_threshold) {
+        const gap = targetAmount - co.position.amount;
+        if (gap > 0) {
+          const confidence = this.confidence(
+            co.position.instrument,
+            context.bar,
+          );
+          if (confidence && confidence > 0) {
+            increase.push(co);
+            continue;
+          }
+        }
+      }
+
+      // Candidates to keep unchanged
+      keep.push(co);
+    }
+
+    return [close, increase, keep];
+  }
+
+  /** Sort each instrument into one of these categories:
+   * - Open: Open new position
+   * - Increase: Increase size of existing position
+   * - Pass: Instrument is not desired
+   */
+  private open_classification(
+    context: StrategyContext,
+  ): [PurchaseOrders, PurchaseOrders, PurchaseOrders] {
+    const open: PurchaseOrders = [];
+    const increase: PurchaseOrders = [];
+    const pass: PurchaseOrders = [];
+    const targetAmount = context.value * this.position_size;
+
+    for (const po of context.purchaseorders) {
+      const timing = this.timing(po.instrument, context.bar);
+      const confidence = this.confidence(po.instrument, context.bar);
+
+      // Desirable position
+      if (timing <= this.buy_threshold && confidence && confidence > 0) {
+        // TODO: Is it ok to compare instrument, or is it necessary to compare symbol instead?
+        const existing = context.closeorders.find((co: CloseOrder) =>
+          co.position.instrument === po.instrument
+        );
+        if (existing) {
+          const gap = targetAmount - existing.position.amount;
+          if (gap > 0) {
+            increase.push(po);
+            continue;
+          }
+        } else {
+          open.push(po);
+          continue;
+        }
+      }
+
+      pass.push(po);
+    }
+    return [open, increase, pass];
+  }
+
+  /** List of open positions that should closed when the time is right */
+  private open_keep(context: StrategyContext): CloseOrders {
+    return context.closeorders.filter((co: CloseOrder) =>
+      this.timing(co.position.instrument, context.bar) < this.sell_threshold
+    )
+      .filter((co: CloseOrder) => {
+        this.confidence(co.position.instrument, context.bar) !== undefined;
+      });
+  }
+
+  // Indentify positions that are
+  // - Open, and want to close
+  // - Open, and don't want to close (yet)
+  // - Open, and want to increase
+  // - New not yet open
 
   public open(context: StrategyContext): PurchaseOrders {
     // const investors = this.investors(context);
@@ -136,6 +236,7 @@ export class Policy implements Strategy {
     // TODO:
     // - Only keep records that have posisitve gap
     // - Combine with the purchase orders
+    // - Skip orders that are already decided to close
     // - Sort by gap
 
     const cdf = DataFrame.fromRecords(crec);
