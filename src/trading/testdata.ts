442bfaa353316c215d581f57a8edd41a24804761
Søren Døssing <netcom@sauber.net>
2025-02-17 13:49:24 +0100

    Ensure data is loaded only once

--- src/trading/testdata.ts
+++ src/trading/testdata.ts
@@ -2,10 +2,11 @@ import { Instrument, StrategyContext } from "@sauber/backtest";
 import { Assets } from "📚/assets/mod.ts";
 import { path } from "📚/assets/testdata.ts";
 import { Loader } from "📚/trading/loader.ts";
+import { InvestorRanking } from "📚/ranking/mod.ts";
 
 export const assets = Assets.disk(path);
 export const investor = await assets.community.any();
-export const rankModel = assets.ranking;
+export const rankModel: InvestorRanking = assets.ranking;
 await rankModel.load();
 const loader = new Loader(assets);
 export const context: StrategyContext = await loader.strategyContext();
@@ -15,13 +16,11 @@ export const timeModel = await assets.timing();
 // Calculate a dummy ranking score based on length of username.
 export const test_ranking = (instr: Instrument) => {
   const score: number = (instr.symbol.length - 11) * 0.2;
-  // console.log(instr.symbol, "ranking score is", score);
   return score;
 };
 
 // Calculate a dummy timing score based on first letter
 export const test_timing = (instr: Instrument) => {
   const score: number = (instr.symbol.toUpperCase().charCodeAt(0) - 78) / 13;
-  // console.log(instr.symbol, "timing score is", score);
   return score;
 };
