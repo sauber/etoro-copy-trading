c671536b56c1ec71ce7be81539014dd2a3aa09f4
Søren Døssing <netcom@sauber.net>
2025-02-18 22:36:29 +0100

    Target amount depends on rank

--- src/trading/classifier.ts
+++ src/trading/classifier.ts
@@ -81,9 +81,11 @@ export class Classifier {
     return candidates.map((candidate: Candidate) => {
       const pos = candidate.purchaseorders;
       const po: PurchaseOrder = pos[0];
+      // return po;
       // Change amount to gap
-      const gap: number = candidate.gap;
-      const changed: PurchaseOrder = Object.assign({}, po, { amount: gap });
+      // const gap: number = candidate.gap;
+      const amount: Amount = candidate.BuyAmount;
+      const changed: PurchaseOrder = Object.assign({}, po, { amount });
       return changed;
     });
   }
