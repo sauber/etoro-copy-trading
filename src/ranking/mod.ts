39c105f5b2fdf1c6e57ecff5b7c4b2d1eb4d6b77
Søren Døssing <netcom@sauber.net>
2025-02-18 18:42:41 +0000

    Export InvestorRanking

--- src/ranking/mod.ts
+++ src/ranking/mod.ts
@@ -4,6 +4,7 @@ import { Investor } from "📚/investor/mod.ts";
 export * from "./investor-ranking.ts";
 export * from "📚/ranking/ranking-cache.ts";
 export * from "📚/ranking/ranking-strategy.ts";
+export * from "📚/ranking/investor-ranking.ts";
 
 export interface Ranking {
   // Rank of investor at bar as single numeric value
