3e36b5f1a586b62b37d546286bbb693878711f2f
Søren Døssing <netcom@sauber.net>
2025-06-19 20:57:36 +0200

    Add useragent to fetch

--- src/repository/fetch-json.ts
+++ src/repository/fetch-json.ts
@@ -6,6 +6,8 @@ export function fetchjson(url: string): Promise<JSONObject> {
   return fetch(url, {
     headers: {
       accept: "application/json",
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
     },
   }).then((resp) => resp.json());
 }
