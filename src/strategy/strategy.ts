fa878fa5e1caf5aad6b385ae252e6cf878ac3654
Søren Døssing <netcom@sauber.net>
2025-12-05 08:51:20 +0000

    Reduce stoploss limit

--- src/strategy/strategy.ts
+++ src/strategy/strategy.ts
@@ -38,7 +38,7 @@ export type StrategyParameters = {
 export const inputParameters: Limits = {
   weekday: { min: 0, max: 6, default: 1, int: true },
   position_size: { min: 0.005, max: 0.2, default: 0.07 },
-  stoploss: { min: 0.05, max: 0.95, default: 0.85 },
+  stoploss: { min: 0.80, max: 0.95, default: 0.85 },
   limit: { min: 1, max: 15, default: 3, int: true },
 };
 type Input = Record<keyof typeof inputParameters, number>;
